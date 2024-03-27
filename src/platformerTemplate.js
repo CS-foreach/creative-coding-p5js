82a120e3af47aa3b1e6c636e979dd4df2b33ec1c
NLChung9 <55558755+NewtonLC@users.noreply.github.com>
2024-03-27 13:45:46 -0700

    Made some progress
    Added gravity, player controls, one platform, some collision detection
    
    Major work in progress

--- src/platformerTemplate.js
+++ src/platformerTemplate.js
@@ -1,60 +1,107 @@
+class Player {
+    constructor(){
+        this.r = 50;
+        this.x = this.r;
+        this.y = this.r;
+        this.jumpPower = 5;
+        this.runSpeed = 5;
+
+        this.canJump = true;
+        this.y_speed = 0;
+    }
+
+    check_on_land(){
+        if (get(this.x, this.bottom()+1).every((val,index) => val === bg_color_array[index])){        //If this player is in the air...
+            this.canJump = false;                                                                     //this player cannot jump.
+            return false;
+        }
+        else{
+            this.canJump = true;
+            return true;
+        }
+    }
+
+    gravity(){
+        if (this.y_speed < 10){
+            this.y_speed += gravity_strength;
+        }
+
+        //if (this.check_on_land() == false){
+            this.y += min(this.y_speed,window.innerHeight-this.bottom());
+        //}
+    }
+
+    draw(){
+        circle(this.x, this.y, 2*this.r);
+    }
+
+    top(){
+        return this.y - this.r;
+    }
+
+    bottom(){
+        return this.y + this.r;
+    }
+}
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    
+
     // focusing the sketch means keypresses
     // will work without you needing to "activate"
     // it by clicking on it
     this.focus();
-  }
+}
   
-  let r = 50;
-  let x = r;
-  let y = r;
+let Player1 = new Player();
+let Player2 = new Player();
   
-  let r2 = 50;
-  let x2 = r2;
-  let y2 = r2;
-  
-  function draw() {
-    background("black");
-    
+let background_color = 'rgba(0,0,0,255)';
+let bg_color_array = [0,0,0,255];
+function draw() {
+    Player1.gravity();
+    Player2.gravity();
+    running();
+
+    background(background_color);
+
     fill("red");
-    circle(x, y, 2*r);
-    
+    Player1.draw();
+
     fill("blue");
-    circle(x2, y2, 2*r);
-  }
-  
-  function keyPressed() {
-    if (key == "w") {
-      y = y - r;
+    Player2.draw();
+
+    fill("yellow");
+    rect(100, 500, 500, 200);
+
+    textSize(50);
+    fill('white');
+    text(Player1.canJump, 50, 50);
+}
+
+let gravity_strength = 0.1;
+
+function keyPressed() {
+    if (key == "w" & Player1.canJump) {     //Replace canjump with check on land
+        text(Player1.canJump, 50, 50);
+        Player1.y_speed = -Player1.jumpPower;
     }
-    
-    if (key == "a") {
-      x = x - r;
+    if (keyCode === UP_ARROW & Player2.canJump) {
+        Player2.y_speed = -Player2.jumpPower;
     }
-    
-    if (key == "s") {
-      y = y + r;
+}
+
+function running(){
+    if (keyIsDown(65)){         //The ASCII keycode for "a" is 65.
+        Player1.x -= Player1.runSpeed;
     }
-    
-    if (key == "d") {
-      x = x + r;
+    if (keyIsDown(68)){         //The ASCII keycode for "d" is 68.
+        Player1.x += Player1.runSpeed;
     }
-    
-    if (keyCode === UP_ARROW) {
-      y2 = y2 - r2;
+    if (keyIsDown(LEFT_ARROW)){
+        Player2.x -= Player2.runSpeed;
     }
-    
-    if (keyCode === LEFT_ARROW) {
-      x2 = x2 - r2;
+    if (keyIsDown(RIGHT_ARROW)){
+        Player2.x += Player2.runSpeed;
     }
-    
-    if (keyCode === DOWN_ARROW) {
-      y2 = y2 + r2;
-    }
-    
-    if (keyCode === RIGHT_ARROW) {
-      x2 = x2 + r2;
-    }
-  }
+}
