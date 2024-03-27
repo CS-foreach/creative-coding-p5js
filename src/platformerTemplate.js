51f08e5c3f3c23b0d14f59c29b332d4ef1d09714
phentos <40075433+phentos@users.noreply.github.com>
2024-03-27 13:45:44 -0700

    initial skeleton

--- src/platformerTemplate.js
+++ src/platformerTemplate.js
@@ -1,60 +1,29 @@
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-    
-    // focusing the sketch means keypresses
-    // will work without you needing to "activate"
-    // it by clicking on it
+    createCanvas(400, 300);
     this.focus();
   }
   
-  let r = 50;
-  let x = r;
-  let y = r;
-  
-  let r2 = 50;
-  let x2 = r2;
-  let y2 = r2;
-  
-  function draw() {
-    background("black");
-    
-    fill("red");
-    circle(x, y, 2*r);
-    
-    fill("blue");
-    circle(x2, y2, 2*r);
+
+function draw() {
+  drawLevel();
+  drawPlayers();
+}
+
+function drawLevel() {}
+function drawPlayers() {}
+function jump(playerNumber) {}
+function move(playerNumber, direction) {}
+
+function keyPressed() {
+  if (key == " ") {
+    jump(1);
   }
   
-  function keyPressed() {
-    if (key == "w") {
-      y = y - r;
-    }
-    
-    if (key == "a") {
-      x = x - r;
-    }
-    
-    if (key == "s") {
-      y = y + r;
-    }
-    
-    if (key == "d") {
-      x = x + r;
-    }
-    
-    if (keyCode === UP_ARROW) {
-      y2 = y2 - r2;
-    }
-    
-    if (keyCode === LEFT_ARROW) {
-      x2 = x2 - r2;
-    }
-    
-    if (keyCode === DOWN_ARROW) {
-      y2 = y2 + r2;
-    }
-    
-    if (keyCode === RIGHT_ARROW) {
-      x2 = x2 + r2;
-    }
+  if (key == "a") {
+    move(1, 'left');
   }
+      
+  if (key == "d") {
+    move(1, 'right');
+  }
+}
