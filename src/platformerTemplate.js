9175269bbbbc393926cd8f91ce15324f113bd5cb
phentos <40075433+phentos@users.noreply.github.com>
2024-03-28 00:12:13 -0700

    remove hardcode of `bg_color_array`

--- src/platformerTemplate.js
+++ src/platformerTemplate.js
@@ -13,7 +13,7 @@ let playerAcceleration = 0.5;
 let playerTopSpeed = 10;
 
 // How high the player can jump
-let jumpForce = -playerHeight/2 + 1;
+let jumpForce = -playerHeight / 2 + 1;
 
 // How fast the player is moving now
 let playerVelocityY = 0;
@@ -48,23 +48,22 @@ function checkInputs() {
   }
 }
 
-bg_color = 'rgba(0,0,0,255)';
-bg_color_array = [0,0,0,255];
+let bg_color = "black";
 function drawLevel() {
   background(bg_color);
-  
+
   defaultLevel();
 
   drawExit(levelExit_X, levelExit_Y, levelExit_Size);
 }
 
 function defaultLevel() {
-  drawFloor(50,250,60, "grey");
-  drawFloor(150,200,30, "grey");
+  drawFloor(50, 250, 60, "grey");
+  drawFloor(150, 200, 30, "grey");
 
-  drawTrickyFloor(50,170,50);
-  drawTrickyFloor(137,141,50);
-  drawBlock(280,90);
+  drawTrickyFloor(50, 170, 50);
+  drawTrickyFloor(137, 141, 50);
+  drawBlock(280, 90);
 }
 
 function drawFloor(x, y, w, c) {
@@ -73,7 +72,7 @@ function drawFloor(x, y, w, c) {
 }
 
 function drawTrickyFloor(x, y, w) {
-  fill('lightblue');
+  fill("lightblue");
   rect(x, y, w, 4);
 }
 
@@ -108,25 +107,30 @@ function newGame() {
   player_won = false;
 }
 
-function drawExit(x,y,size){
-  textSize(size/3);
+function drawExit(x, y, size) {
+  textSize(size / 3);
   textAlign(CENTER, CENTER);
-  text("😀", x + size/2, y + size/2);
-  if (x <= playerX & playerX <= x+size & y <= playerY & playerY <= y+size){
+  text("😀", x + size / 2, y + size / 2);
+  if (
+    (x <= playerX) &
+    (playerX <= x + size) &
+    (y <= playerY) &
+    (playerY <= y + size)
+  ) {
     YouWin();
   }
 }
 
 let player_won = false;
-function YouWin(){
+function YouWin() {
   player_won = true;
   textSize(75);
   fill("yellow");
-  text("YOU WIN!!!", width/2, height/2);
+  text("YOU WIN!!!", width / 2, height / 2);
   textSize(20);
   fill("white");
-  text("Press space to restart.", width/2, height * 0.75);
-  text("Change what happens when you win!", width/2, height * 0.9);
+  text("Press space to restart.", width / 2, height * 0.75);
+  text("Change what happens when you win!", width / 2, height * 0.9);
   playerVelocityX = 0;
   playerVelocityY = 0;
 }
@@ -134,7 +138,7 @@ function YouWin(){
 function keyPressed() {
   if (key == " ") {
     move("up");
-    if(player_won){
+    if (player_won) {
       newGame();
     }
   }
@@ -152,61 +156,87 @@ function applyPhysics() {
   playerVelocityY += gravity;
   check_collision("Y");
   playerY += playerVelocityY;
-  
+
   playerVelocityX = constrain(playerVelocityX, -playerTopSpeed, playerTopSpeed);
   check_collision("X");
   playerX += playerVelocityX;
 }
 
-function check_collision(direction){
-  if(direction == "X"){
-    if(playerVelocityX < 0){                                     //When velocity is less than 0, the player is moving left.
-      if(isTouching("left")){          //Check if there is an object on the player's left
-        if(-playerVelocityX < 2){
+function check_collision(direction) {
+  if (direction == "X") {
+    if (playerVelocityX < 0) {
+      //When velocity is less than 0, the player is moving left.
+      if (isTouching("left")) {
+        //Check if there is an object on the player's left
+        if (-playerVelocityX < 2) {
+          playerVelocityX = 0;
+        }
+        playerVelocityX = -playerVelocityX * 0.5;
+      }
+    } else if (playerVelocityX > 0) {
+      //When velocity is more than 0, the player is moving right.
+      if (isTouching("right")) {
+        //Check if there is an object on the player's right
+        if (playerVelocityX < 2) {
           playerVelocityX = 0;
         }
         playerVelocityX = -playerVelocityX * 0.5;
       }
     }
-    else if(playerVelocityX > 0){                                //When velocity is more than 0, the player is moving right.
-      if(isTouching("right")){         //Check if there is an object on the player's right
-        if(playerVelocityX < 2){
-          playerVelocityX = 0;
-        }
-        playerVelocityX = -playerVelocityX * 0.5;
-      }                           
-    }
-  }
-  else{
-    if(playerVelocityY < 0){                                     //When velocity is less than 0, the player is moving left.
-      if(isTouching("top")){          //Check if there is an object touching the player's head
+  } else {
+    if (playerVelocityY < 0) {
+      //When velocity is less than 0, the player is moving left.
+      if (isTouching("top")) {
+        //Check if there is an object touching the player's head
         playerVelocityY = -playerVelocityY;
       }
     }
-    if(playerVelocityY > 0){
-      if(isTouching("bottom")){        //Check if there is an object directly under the player
-        playerVelocityY = 0; 
+    if (playerVelocityY > 0) {
+      if (isTouching("bottom")) {
+        //Check if there is an object directly under the player
+        playerVelocityY = 0;
       }
     }
   }
 }
 
-function isTouching(direction){
-  if (direction == "top"){
-    return !get(playerX, playerY).every((val,index) => val === bg_color_array[index]) |
-           !get(playerX + playerWidth/2, playerY).every((val,index) => val === bg_color_array[index]) |
-           !get(playerX + playerWidth, playerY).every((val,index) => val === bg_color_array[index]);
+function isTouching(direction) {
+  let bg_color_array = color(bg_color).levels;
+  if (direction == "top") {
+    return (
+      !get(playerX, playerY).every(
+        (val, index) => val === bg_color_array[index]
+      ) |
+      !get(playerX + playerWidth / 2, playerY).every(
+        (val, index) => val === bg_color_array[index]
+      ) |
+      !get(playerX + playerWidth, playerY).every(
+        (val, index) => val === bg_color_array[index]
+      )
+    );
   }
-  if (direction == "bottom"){
-    return !get(playerX, playerY + playerHeight).every((val,index) => val === bg_color_array[index]) |
-           !get(playerX + playerWidth/2, playerY + playerHeight).every((val,index) => val === bg_color_array[index]) | 
-           !get(playerX + playerWidth, playerY + playerHeight).every((val,index) => val === bg_color_array[index]);
+  if (direction == "bottom") {
+    return (
+      !get(playerX, playerY + playerHeight).every(
+        (val, index) => val === bg_color_array[index]
+      ) |
+      !get(playerX + playerWidth / 2, playerY + playerHeight).every(
+        (val, index) => val === bg_color_array[index]
+      ) |
+      !get(playerX + playerWidth, playerY + playerHeight).every(
+        (val, index) => val === bg_color_array[index]
+      )
+    );
   }
-  if (direction == "left"){
-    return !get(playerX, playerY + playerHeight/2).every((val,index) => val === bg_color_array[index]);
+  if (direction == "left") {
+    return !get(playerX, playerY + playerHeight / 2).every(
+      (val, index) => val === bg_color_array[index]
+    );
   }
-  if (direction == "right"){
-    return !get(playerX + playerWidth, playerY + playerHeight/2).every((val,index) => val === bg_color_array[index]);;
+  if (direction == "right") {
+    return !get(playerX + playerWidth, playerY + playerHeight / 2).every(
+      (val, index) => val === bg_color_array[index]
+    );
   }
 }
 
@@ -224,5 +254,5 @@ function move(direction) {
 }
 
 function mouseClicked() {
-  console.log(`${mouseX}, ${mouseY}`)
+  console.log(`${mouseX}, ${mouseY}`);
 }
