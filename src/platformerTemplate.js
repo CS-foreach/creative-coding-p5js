3144c591168c16d64ea29ec1f8e0506e20e75fcf
phentos <40075433+phentos@users.noreply.github.com>
2024-03-30 23:23:16 -0700

    #17 first physics pass, convert to lerp + `rectMode(CORNERS)`

--- src/platformerTemplate.js
+++ src/platformerTemplate.js
@@ -7,91 +7,93 @@ let playerHeight = 20;
 let playerWidth = 20;
 
 // How fast a player speeds up left or right
-let playerAcceleration = 0.5;
+let playerAcceleration = 2;
 
 // How fast a player can move
 let playerTopSpeed = 10;
 
 // How high the player can jump
-let jumpForce = -playerHeight / 2 + 1;
+let jumpForce = -40;
 
 // How fast the player is moving now
 let playerVelocityY = 0;
 let playerVelocityX = 0;
 
 // How fast the player falls
-let gravity = 0.8;
+let gravity = 0.2;
 
 // How slippery moving is
-let groundFriction = 0.8;
-let airFriction = 0.99;
+let groundFriction = 0.4;
+let airFriction = 0.06;
 
 function setup() {
-  createCanvas(400, 300);
-  this.focus();
-  newGame();
+	createCanvas(400, 300);
+
+	if (debugMode) {
+		frameRate(15);
+	}
+
+	this.focus();
+	rectMode(CORNERS);
+	newGame();
 }
 
+let debugMode = true;
 function draw() {
-  checkInputs();
-  drawLevel();
-  drawPlayer();
+	checkInputs();
+	drawLevel();
+	applyPhysics();
+	drawPlayer();
+	if (debugMode) {
+		drawDebugInfo();
+	}
 }
 
 function checkInputs() {
-  if (keyIsDown(65)) {
-    move("left");
-  } else if (keyIsDown(68)) {
-    move("right");
-  } else {
-    applyFriction();
-  }
+	if (keyIsDown(65)) {
+		move("left");
+	}
+
+	if (keyIsDown(68)) {
+		move("right");
+	}
 }
 
 let bg_color = "black";
 function drawLevel() {
-  background(bg_color);
+	background(bg_color);
 
-  defaultLevel();
+	defaultLevel();
 
-  drawExit(levelExit_X, levelExit_Y, levelExit_Size);
+	drawExit(levelExit_X, levelExit_Y, levelExit_Size);
 }
 
 function defaultLevel() {
-  drawFloor(50, 250, 60, "grey");
-  drawFloor(150, 200, 30, "grey");
+	drawFloor(50, 250, 60, "grey");
+	drawFloor(150, 200, 30, "grey");
 
-  drawTrickyFloor(50, 170, 50);
-  drawTrickyFloor(137, 141, 50);
-  drawBlock(280, 90);
+	drawTrickyFloor(50, 170, 50);
+	drawTrickyFloor(137, 141, 50);
 }
 
 function drawFloor(x, y, w, c) {
-  fill(c);
-  rect(x, y, w, 10);
+	fill(c);
+	rect(x, y, x + w, y + 10);
 }
 
 function drawTrickyFloor(x, y, w) {
-  fill("lightblue");
-  rect(x, y, w, 4);
+	fill("lightblue");
+	rect(x, y, x + w, y + 4);
 }
 
 function drawBlock(x, y) {
-  fill("darkred");
-  rect(x, y, 30, 30);
+	fill("darkred");
+	rect(x, y, x + 30, y + 30);
 }
 
 function drawPlayer() {
-  applyPhysics();
-  checkBoundaries();
-
-  fill("red");
-  rect(playerX, playerY, playerWidth, playerHeight);
-}
-
-function checkBoundaries() {
-  playerX = constrain(playerX, 0, width - playerWidth);
-  playerY = constrain(playerY, 0, height - playerHeight);
+	fill("red");
+	rect(playerX, playerY, playerX + playerWidth, playerY - playerHeight);
 }
 
 //Where the level exit icon is
@@ -99,160 +101,116 @@ let levelExit_X;
 let levelExit_Y;
 let levelExit_Size;
 function newGame() {
-  playerY = height - playerHeight;
-  playerX = width / 2;
-  levelExit_X = 255;
-  levelExit_Y = 70;
-  levelExit_Size = 75;
-  player_won = false;
+	playerY = height;
+	playerX = width / 2;
+	levelExit_X = 255;
+	levelExit_Y = 70;
+	levelExit_Size = 75;
+	player_won = false;
+	airborn = false;
 }
 
 function drawExit(x, y, size) {
-  textSize(size / 3);
-  textAlign(CENTER, CENTER);
-  text("😀", x + size / 2, y + size / 2);
-  if (
-    (x <= playerX) &
-    (playerX <= x + size) &
-    (y <= playerY) &
-    (playerY <= y + size)
-  ) {
-    YouWin();
-  }
+	textSize(size / 3);
+	textAlign(CENTER, CENTER);
+	text("😀", x + size / 2, y + size / 2);
+	if (
+		(x <= playerX) &
+		(playerX <= x + size) &
+		(y <= playerY) &
+		(playerY <= y + size)
+	) {
+		YouWin();
+	}
 }
 
 let player_won = false;
 function YouWin() {
-  player_won = true;
-  textSize(75);
-  fill("yellow");
-  text("YOU WIN!!!", width / 2, height / 2);
-  textSize(20);
-  fill("white");
-  text("Press space to restart.", width / 2, height * 0.75);
-  text("Change what happens when you win!", width / 2, height * 0.9);
-  playerVelocityX = 0;
-  playerVelocityY = 0;
+	player_won = true;
+	textSize(75);
+	fill("yellow");
+	text("YOU WIN!!!", width / 2, height / 2);
+	textSize(20);
+	fill("white");
+	text("Press space to restart.", width / 2, height * 0.75);
+	text("Change what happens when you win!", width / 2, height * 0.9);
+	playerVelocityX = 0;
+	playerVelocityY = 0;
 }
 
 function keyPressed() {
-  if (key == " ") {
-    move("up");
-    if (player_won) {
-      newGame();
-    }
-  }
+	if (key == " ") {
+		move("up");
+		if (player_won) {
+			newGame();
+		}
+	}
+}
+
+function drawDebugInfo() {
+	fill("white");
+	textSize(10);
+	noStroke();
+	textAlign(LEFT);
+
+	let [opX, opY] = [playerVelocityX, playerVelocityY].map((value) =>
+		value < 0 ? " - " : " + "
+	);
+
+	let [X, Y, vY, vX] = [playerX, playerY, playerVelocityY, playerVelocityX].map(
+		(value) => abs(value).toFixed(2)
+	);
+
+	text(`Y: ${Y}${opY}${vY}`, 5, 10);
+	text(`X: ${X}${opX}${vX}`, 5, 20);
+	text(`airborn: ${airborn}`, 5, 30);
 }
 
 function applyFriction() {
-  if (playerVelocityY) {
-    playerVelocityX *= airFriction;
-  } else {
-    playerVelocityX *= groundFriction;
-  }
+	playerVelocityX = lerp(
+		playerVelocityX,
+		0,
+		airborn ? airFriction : groundFriction
+	);
 }
 
+let airborn;
 function applyPhysics() {
-  playerVelocityY += gravity;
-  check_collision("Y");
-  playerY += playerVelocityY;
+	playerVelocityX = constrain(playerVelocityX, -playerTopSpeed, playerTopSpeed);
+	playerVelocityY = airborn
+		? lerp(playerVelocityY, playerTopSpeed, gravity)
+		: 0;
 
-  playerVelocityX = constrain(playerVelocityX, -playerTopSpeed, playerTopSpeed);
-  check_collision("X");
-  playerX += playerVelocityX;
+	applyFriction();
+	enforceCollisions();
+
+	playerX += playerVelocityX;
+	playerY += playerVelocityY;
+
+	checkBoundaries();
 }
 
-function check_collision(direction) {
-  if (direction == "X") {
-    if (playerVelocityX < 0) {
-      //When velocity is less than 0, the player is moving left.
-      if (isTouching("left")) {
-        //Check if there is an object on the player's left
-        if (-playerVelocityX < 2) {
-          playerVelocityX = 0;
-        }
-        playerVelocityX = -playerVelocityX * 0.5;
-      }
-    } else if (playerVelocityX > 0) {
-      //When velocity is more than 0, the player is moving right.
-      if (isTouching("right")) {
-        //Check if there is an object on the player's right
-        if (playerVelocityX < 2) {
-          playerVelocityX = 0;
-        }
-        playerVelocityX = -playerVelocityX * 0.5;
-      }
-    }
-  } else {
-    if (playerVelocityY < 0) {
-      //When velocity is less than 0, the player is moving left.
-      if (isTouching("top")) {
-        //Check if there is an object touching the player's head
-        playerVelocityY = -playerVelocityY;
-      }
-    }
-    if (playerVelocityY > 0) {
-      if (isTouching("bottom")) {
-        //Check if there is an object directly under the player
-        playerVelocityY = 0;
-      }
-    }
-  }
+function checkBoundaries() {
+	playerX = constrain(playerX, 0, width - playerWidth);
+	playerY = constrain(playerY, 0, height);
 }
 
-function isTouching(direction) {
-  let bg_color_array = color(bg_color).levels;
-  if (direction == "top") {
-    return (
-      !get(playerX, playerY).every(
-        (val, index) => val === bg_color_array[index]
-      ) |
-      !get(playerX + playerWidth / 2, playerY).every(
-        (val, index) => val === bg_color_array[index]
-      ) |
-      !get(playerX + playerWidth, playerY).every(
-        (val, index) => val === bg_color_array[index]
-      )
-    );
-  }
-  if (direction == "bottom") {
-    return (
-      !get(playerX, playerY + playerHeight).every(
-        (val, index) => val === bg_color_array[index]
-      ) |
-      !get(playerX + playerWidth / 2, playerY + playerHeight).every(
-        (val, index) => val === bg_color_array[index]
-      ) |
-      !get(playerX + playerWidth, playerY + playerHeight).every(
-        (val, index) => val === bg_color_array[index]
-      )
-    );
-  }
-  if (direction == "left") {
-    return !get(playerX, playerY + playerHeight / 2).every(
-      (val, index) => val === bg_color_array[index]
-    );
-  }
-  if (direction == "right") {
-    return !get(playerX + playerWidth, playerY + playerHeight / 2).every(
-      (val, index) => val === bg_color_array[index]
-    );
-  }
-}
+function enforceCollisions() {}
 
 function move(direction) {
-  if (direction == "up" && playerVelocityY == 0) {
-    playerVelocityY += jumpForce;
-  }
+	if (direction == "up" && playerVelocityY == 0) {
+		playerVelocityY += jumpForce;
+		airborn = true;
+	}
 
-  if (direction == "left") {
-    playerVelocityX -= playerAcceleration;
-  }
-  if (direction == "right") {
-    playerVelocityX += playerAcceleration;
-  }
+	if (direction == "left") {
+		playerVelocityX -= playerAcceleration;
+	}
+	if (direction == "right") {
+		playerVelocityX += playerAcceleration;
+	}
 }
 
 function mouseClicked() {
-  console.log(`${mouseX}, ${mouseY}`);
+	console.log(`${mouseX}, ${mouseY}`);
 }
